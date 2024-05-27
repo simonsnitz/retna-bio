9cf90e2f950ea4bec0545fb5db70440a6d22af6a
Simon d'Oelsnitz <simonsnitz@gmail.com>
2024-05-27 09:19:28 -0400

    added skeleton of Services component

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -11,58 +11,89 @@ const Services = forwardRef(function Services(props, ref) {
 
   return (
     
-    // Background with gradient color
+    // Container
     <Grid
       props={props}
       ref={ref}
-      sx={{height: '100vh'}}
       container
       spacing={0}
       columns={12}
       justifyContent="center"
     >
 
-    {/* RamR structure backgorund image */}
-      <Box
-        component="img"
-        id="picture"
-          sx={{
-            width: { xs: "150vw", sm: "80vw" },
-            right: 0,
-            top: { xs:"35vh", sm:0},
-            position: "absolute",
-          }}
-          style={{opacity: "20%", zIndex:-10}}
-          src={sensor}
-      />
-
     <Grid item xs={2} >
+      {/* Spacer */}
     </Grid>
     <Grid item xs={4} >
 
-        {/* Header */}
+        {/* Topic Header */}
       <Typography
-        sx={{ fontSize: { xs: 20, md: 48 }, mt: 30 }} component="div">
+        sx={{ fontSize: { xs: 20, md: 48 }, mt: 20 }} component="div">
         Services
-      </Typography>
-      <Typography
-        sx={{ fontSize: { xs: 14, md: 30 } }} component="div">
-        'list of our services'
-      </Typography>
-
-        {/* Description */}
-      <Typography
-        sx={{ fontSize: { xs: 14, md: 24 }, mt: 15 }} component="div">
-        We provide advice on strategy and genetic design 
       </Typography>
 
       </Grid>
 
     <Grid item xs={6} >
+      {/* Spacer */}
+    </Grid>
+
+
+    {/* Strategy section */}
+    <Grid container columns={12}>
+
+    <Grid item xs={2}>{/* Spacer */}</Grid>
+      <Grid item xs={10}>
+        <Typography
+          sx={{ fontSize: { xs: 24, md: 36 }, mt: 10 }} component="div">
+          Project strategy
+        </Typography>
+      </Grid>
+
+      <Grid item xs={2}>{/* Spacer */}</Grid>
+      <Grid item xs={4}>
+        <Typography  sx={{ fontSize: { xs: 24, md: 36 }, mt: 10 }} style={{border: "1px solid black"}} component="div" textAlign="center">
+          Gene identification
+        </Typography>
+      </Grid>
+      <Grid item xs={4}>
+        <Typography  sx={{ fontSize: { xs: 24, md: 36 }, mt: 10 }} component="div" textAlign="center">
+          Feasibility assessment
+        </Typography>
+      </Grid>
+      <Grid item xs={2}>{/* Spacer */}</Grid>
+
+
+          {/* Genetic Design section */}
+    <Grid container columns={12}>
+
+      <Grid item xs={2}>{/* Spacer */}</Grid>
+      <Grid item xs={10}>
+        <Typography
+          sx={{ fontSize: { xs: 24, md: 36 }, mt: 10 }} component="div">
+          Genetic Design
+        </Typography>
+      </Grid>
+
+      <Grid item xs={2}>{/* Spacer */}</Grid>
+      <Grid item xs={4}>
+        <Typography  sx={{ fontSize: { xs: 24, md: 36 }, mt: 10 }} style={{border: "1px solid black"}} component="div" textAlign="center">
+          Component design
+        </Typography>
+      </Grid>
+      <Grid item xs={4}>
+        <Typography  sx={{ fontSize: { xs: 24, md: 36 }, mt: 10 }} component="div" textAlign="center">
+          Circuit design
+        </Typography>
+      </Grid>
+      <Grid item xs={2}>{/* Spacer */}</Grid>
+
     </Grid>
 
 
 
+
+    </Grid>
     </Grid>
     
   );
