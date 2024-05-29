500ae8ea19b4f0c02c95e030930ef3633d89398f
Simon d'Oelsnitz <simonsnitz@gmail.com>
2024-05-29 09:19:20 -0400

    updated Services and added a contact button to About

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,6 +1,6 @@
 import React, {forwardRef} from "react";
 
-import { Grid, Typography, Box } from '@mui/material/';
+import { Grid, Typography, Box, Button } from '@mui/material/';
 
 import sensor from './sensorStructure.png';
 
@@ -39,19 +39,37 @@ const About = forwardRef(function About(props, ref) {
     </Grid>
     <Grid item xs={4} mt={20}>
 
+    <Typography
+        sx={{ fontSize: { xs: 14, md: 36 }, mt: 10 }} component="div">
+          Synthetic Biology Consulting
+    </Typography>
 
         {/* Description */}
       <Typography
         sx={{ fontSize: { xs: 14, md: 22 }, mt: 10 }} component="div">
-        Retna Bio is a synthetic biology consulting firm managed and operated by 
+        Retna Bio is a consulting firm managed and operated by 
         <a href="https://scholar.google.com/citations?hl=en&user=4lfhK5wAAAAJ" style={{textDecoration: "None", color: "#1971ff"}}
         target="_blank" rel="noopener noreferrer"> Simon d'Oelsnitz</a>
         , a Synthetic Biology Fellow at Harvard Medical School. 
         We specialize in the design and application of <b>chemical-responsive 
         genetic circuits</b> in microbial hosts.
-        Our mission is to help bridge the gap between academic-led innovation and 
+        Our mission is to help bridge the gap between academic innovation and 
         industry-led impact.
       </Typography>
+
+      <Button 
+        variant="contained"
+        style={{marginTop: 35}}
+        >
+        <a href="mailto:simon@retna.bio" style={{textDecoration: "None", color: "white"}}
+          target="_blank" rel="noopener noreferrer"> Get in touch</a>
+          
+      </Button>
+
+      {/* <Typography
+        sx={{ fontSize: { xs: 14, md: 22 }, mt: 7 }} component="div" style={{border: "1px solid black", width: 200}}>
+          Get in touch
+      </Typography> */}
 
       </Grid>
 
--- src/Components/Services.js
+++ src/Components/Services.js
@@ -19,76 +19,111 @@ const Services = forwardRef(function Services(props, ref) {
       spacing={0}
       columns={12}
       justifyContent="center"
+      backgroundColor="#0088CD"
+      // backgroundColor="#9655cf"
     >
+    
+    <Grid item xs={2} >
+      {/* Spacer */}
+    </Grid>
+
+        {/* Topic Header */}
+    <Grid item xs={8} >
+      <Typography
+        sx={{ fontSize: { xs: 20, md: 36 }, mt: 10 }} color={"white"} component="div" textAlign="center">
+        Services
+      </Typography>
+    </Grid>
 
     <Grid item xs={2} >
       {/* Spacer */}
     </Grid>
+
+
+    <Grid container xs={12} columns={12}>
+
+    <Grid item xs={2} >
+      {/* Spacer */}
+    </Grid>
+
     <Grid item xs={4} >
 
-        {/* Topic Header */}
-      <Typography
-        sx={{ fontSize: { xs: 20, md: 48 }, mt: 20 }} component="div">
-        Services
-      </Typography>
+    {/* Feasibility section */}
+      <Grid container  mt={5}>
+          {/* Header */}
+        <Grid item xs={12} textAlign="center">
+          <Typography sx={{ fontSize: { xs: 16, md: 24 }}}  color="white" component="div" >
+          Feasibility Analysis
+        </Typography>
+        </Grid>
 
+        <Grid item xs={12} textAlign="center">
+          <Typography sx={{ fontSize: { xs: 16, md: 30 }}}  component="div" style={{border: "1px solid black"}}>
+          Image
+        </Typography>
+        </Grid>
+
+        <Grid item xs={12} >
+          <Typography sx={{ fontSize: { xs: 12, md: 20 }}}  component="div" color="white" padding={7}>
+            Content
+        </Typography>
+        </Grid>
+        
       </Grid>
+    </Grid>
 
-    <Grid item xs={6} >
+    <Grid item xs={4} >
+
+    {/* Genetic Design section */}
+    <Grid container  mt={5}>
+          {/* Header */}
+        <Grid item xs={12} textAlign="center">
+          <Typography sx={{ fontSize: { xs: 16, md: 24 }}}  component="div" color="white">
+          Genetic Design
+        </Typography>
+        </Grid>
+
+        <Grid item xs={12} textAlign="center">
+          <Typography sx={{ fontSize: { xs: 16, md: 30 }}}  component="div" style={{border: "1px solid black"}}>
+          Image
+        </Typography>
+        </Grid>
+
+        <Grid item xs={12} >
+        <Typography sx={{ fontSize: { xs: 12, md: 20 }}}  component="div" padding={7} color="white">
+          Content
+        </Typography>
+        </Grid>
+        
+      </Grid>
+    </Grid>
+
+
+
+    <Grid item xs={2} >
       {/* Spacer */}
     </Grid>
 
 
-    {/* Feasibility section */}
-    <Grid container columns={12}>
 
-    <Grid item xs={2}>{/* Spacer */}</Grid>
-      <Grid item xs={8}>
+      {/* <Grid item xs={4}>
         <Typography
           sx={{ fontSize: { xs: 24, md: 36 }, mt: 10 }} component="div" textAlign="center">
-          Feasibility assessment
+          Feasibility analysis
         </Typography>
-      </Grid>
+      </Grid> */}
 
-        {/* Feasibility Content */}
-      <Grid container columns={12}>
-        <Grid item xs={2}> 
-            {/* Spacer  */}
-        </Grid>
-      <Grid item xs={4} style={{border: "1px solid black", height: 100}}>
+
+
+
+      {/* <Grid item xs={4} style={{border: "1px solid black", height: 100}}> */}
         {/* Image */}
-      </Grid>
-      <Grid item xs={4} style={{border: "1px solid black", height: 100}}>
-        {/* Text */}
-      </Grid>
-      </Grid>
+      {/* </Grid> */}
 
 
-    {/* Genetic Design section */}
-    <Grid container columns={12}>
 
-      <Grid item xs={2}>{/* Spacer */}</Grid>
-      <Grid item xs={8}>
-        <Typography
-          sx={{ fontSize: { xs: 24, md: 36 }, mt: 10 }} component="div" textAlign="center">
-          Genetic Design
-        </Typography>
-      </Grid>
 
-        {/* Genetic Design Content */}
-        <Grid container columns={12}>
-        <Grid item xs={2}> 
-            {/* Spacer  */}
-        </Grid>
-      <Grid item xs={4} style={{border: "1px solid black", height: 100}}>
-        {/* Image */}
-      </Grid>
-      <Grid item xs={4} style={{border: "1px solid black", height: 100}}>
-        {/* Text */}
-      </Grid>
-      </Grid>
 
-    </Grid>
 
 
 
