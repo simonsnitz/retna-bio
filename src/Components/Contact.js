478d40a3f742fbb5932bff37593ca675a70f27c8
Simon d'Oelsnitz <simonsnitz@gmail.com>
2024-05-29 09:05:29 -0400

    finished MVP of the Contact page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,9 +1,10 @@
 import React, {forwardRef} from "react";
 
-import { Grid, Typography, Box } from '@mui/material/';
+import { Grid, Typography, Link } from '@mui/material/';
 
-import sensor from './sensorStructure.png';
-
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
 
 const Contact = forwardRef(function Contact(props, ref) {
@@ -16,49 +17,54 @@ const Contact = forwardRef(function Contact(props, ref) {
       props={props}
       ref={ref}
       container
-      sx={{height: '100vh'}}
       spacing={0}
       columns={12}
+      mb={20}
       justifyContent="center"
     >
 
-    {/* RamR structure backgorund image */}
-      <Box
-        component="img"
-        id="picture"
-          sx={{
-            width: { xs: "150vw", sm: "80vw" },
-            right: 0,
-            top: { xs:"35vh", sm:0},
-            position: "absolute",
-          }}
-          style={{opacity: "20%", zIndex:-10}}
-          src={sensor}
-      />
 
     <Grid item xs={2} >
     </Grid>
-    <Grid item xs={4} >
+    <Grid item xs={8} textAlign="center">
 
         {/* Header */}
       <Typography
-        sx={{ fontSize: { xs: 20, md: 48 }, mt: 30 }} component="div">
+        sx={{ fontSize: { xs: 20, md: 36 }, mt: 15 }} component="div" >
         Contact
       </Typography>
       <Typography
-        sx={{ fontSize: { xs: 14, md: 30 } }} component="div">
-        Reach out for references and to learn more about our services.
+        sx={{ fontSize: { xs: 14, md: 22 } }} mt={2} mb={5} component="div">
+        Please reach out by email for references and to learn more about our services.
       </Typography>
 
-        {/* Description */}
+        {/* Email */}
+      <Grid container justifyContent={"center"} sx={{height: 30}} >
+      <MailOutlineIcon sx={{ height: "100%", display: "inline", mr:1 }} />
       <Typography
-        sx={{ fontSize: { xs: 14, md: 24 }, mt: 15 }} component="div">
-        contact info
+        sx={{ fontSize: { xs: 14, md: 20 }, display: "inline" }} component="div">
+        <b>Email: </b>
+        <a href="mailto:simon@retna.bio" style={{textDecoration:"none", color: "#005ac2"}} target="_blank" rel="noopener noreferrer"> simon@retna.bio</a>
       </Typography>
+      </Grid>
+
+      <Grid container justifyContent={"center"} mt={10}>
+        {/* Social media logos */}
+        <Link
+          href="https://www.linkedin.com/company/retnabio/"
+          target="_blank">
+          <LinkedInIcon sx={{ width: '50px', height: '50px' }} />
+        </Link>
+        <Link
+          href="https://x.com/retnabio"
+          target="_blank">
+          <TwitterIcon sx={{ width: '50px', height: '50px', ml:3 }} />
+        </Link>
+      </Grid>
 
       </Grid>
 
-    <Grid item xs={6} >
+    <Grid item xs={2} >
     </Grid>
 
 
